8488edeb734464f09b309b700414e110a8f0833c
shashank-madan <shashank.um.xl@gmail.com>
2025-03-25 21:12:32 -0400

    added path to index.py

--- backend/api/index.py
+++ backend/api/index.py
@@ -6,6 +6,7 @@ from .routes.auth import router as auth_router
 from .routes.events import router as events_router
 from .database.mongodb import MongoDB
 from contextlib import asynccontextmanager
+from .routes.license_permit_route import router as lp_router
 
 
 @asynccontextmanager
@@ -27,6 +28,7 @@ app.add_middleware(
 add_langgraph_route(app, assistant_ui_graph, "/api/chat")
 app.include_router(auth_router, prefix="/api/auth")
 app.include_router(events_router, prefix="/api/events", tags=["events"])
+app.include_router(lp_router, prefix="/api/events", tags=["lp"])
 
 if __name__ == "__main__":
     import uvicorn
